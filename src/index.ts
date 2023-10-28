12db9fed1c75a866324523b4ddc89da27d1ca11f
Nate Sire <nate.sire@fairwaymc.com>
2023-10-28 03:58:59 -0400

    type module

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+import fetch from 'node-fetch';
 export default class Frontend {
   constructor() {
     console.log('Frontend constructor');
   }
 
-  fetch() {
-    console.log('Frontend fetch');
+  fetch(url?: string) {
+    // node fetch get url
+    fetch('http://localhost:3000/', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
   }
 }
